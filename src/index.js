060bf6ba7bf7f78a17ef859f6ddc323ec5270732
khassangali Gapparov <hasangaly@gmail.com>
2019-03-04 02:42:27 +0600

    solve attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  var status = true;
+  var checkStatus = 0;
+  var buffer=[];
+  for (i = 0; i < bracketsConfig.length; i++){
+    buffer[i]=0;
+  }
+  for (j =0; j < str.length; j++){
+    for (i = 0; i < bracketsConfig.length; i++){
+      var index=bracketsConfig[i].indexOf(str[j]);
+      if ((index > -1)&(index%2==0)){
+        buffer[i]++;
+      }
+      if ((index > -1)&(index%2==1)){
+        buffer[i]--;
+
+        if (buffer[i] < 0){
+          status = false;
+        }
+      }
+    }
+  }
+  buffer.forEach(function(val){
+   if (val != 0){
+     status = false;
+   }
+  });
+ return status;
+};
