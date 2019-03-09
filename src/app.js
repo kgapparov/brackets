03786367a8c9c9d0f2e8d8035a54db9170e4403c
khassangali Gapparov <hasangaly@gmail.com>
2019-03-10 02:24:04 +0600

    fix: updated other aproach

---
+++ src/app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+function check(str, bracketsConfig) {
+  // your solution
+var bracket = {
+  lastBracket:[],
+  expectingClose:[],
+  openingBracket:[],
+  closingBracket:[],
+  openCount:0,
+  closeCount:0
+};
+var index = -1;
+// getting pattern of open and close brackets
+for (i=0; i < bracketsConfig.length; i++){
+  bracket.openingBracket.push(bracketsConfig[i][0]);
+  bracket.closingBracket.push(bracketsConfig[i][1]);
+}
+for (j=0; j<str.length; j++){
+  if ((bracket.lastBracket==[])&(bracket.closingBracket.includes(str[j]))){
+    return false;
+  }
+  if (bracket.openingBracket.indexOf(str[j]) > -1){
+    bracket.openCount ++;
+    bracket.lastBracket = str[j];
+    index = bracket.openingBracket.indexOf(str[j]);
+    bracket.expectingClose.push(bracket.closingBracket[index]);
+    continue;
+  }
+  if ((bracket.closingBracket.includes(str[j]))&(bracket.expectingClose[bracket.expectingClose.length-1] == str[j])) {
+    bracket.closeCount++;
+    bracket.expectingClose.pop();
+    continue;
+  }
+   if ((bracket.closingBracket.includes(str[j]))&(bracket.expectingClose[bracket.expectingClose.length-1] != str[j])){
+     return false;
+   }
+}
+if (bracket.openCount == bracket.closeCount){
+  return true;
+}
+else {
+  console.log(bracket);
+  return false;
+}
+}
+
+console.log(check('||', [['|', '|']]));
--- src/index.js
+++ src/index.js
@@ -1,30 +1,47 @@
+  //jshint esversion:6
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  var status = true;
-  var checkStatus = 0;
-  var buffer=[];
-  for (i = 0; i < bracketsConfig.length; i++){
-    buffer[i]=0;
-  }
-  for (j =0; j < str.length; j++){
-    for (i = 0; i < bracketsConfig.length; i++){
-      var index=bracketsConfig[i].indexOf(str[j]);
-      if ((index > -1)&(index%2==0)){
-        buffer[i]++;
-      }
-      if ((index > -1)&(index%2==1)){
-        buffer[i]--;
 
-        if (buffer[i] < 0){
-          status = false;
-        }
-      }
-    }
+
+    // your solution
+  var bracket = {
+    lastBracket:[],
+    expectingClose:[],
+    openingBracket:[],
+    closingBracket:[],
+    openCount:0,
+    closeCount:0
+  };
+  var index = -1;
+  // getting pattern of open and close brackets
+  for (i=0; i < bracketsConfig.length; i++){
+    bracket.openingBracket.push(bracketsConfig[i][0]);
+    bracket.closingBracket.push(bracketsConfig[i][1]);
   }
-  buffer.forEach(function(val){
-   if (val != 0){
-     status = false;
-   }
-  });
- return status;
-};
+  for (j=0; j<str.length; j++){
+    if ((bracket.lastBracket==[])&(bracket.closingBracket.includes(str[j]))){
+      return false;
+    }
+    if (bracket.openingBracket.indexOf(str[j]) > -1){
+      bracket.openCount ++;
+      bracket.lastBracket = str[j];
+      index = bracket.openingBracket.indexOf(str[j]);
+      bracket.expectingClose.push(bracket.closingBracket[index]);
+      continue;
+    }
+    if ((bracket.closingBracket.includes(str[j]))&(bracket.expectingClose[bracket.expectingClose.length-1] == str[j])) {
+      bracket.closeCount++;
+      bracket.expectingClose.pop();
+      continue;
+    }
+     if ((bracket.closingBracket.includes(str[j]))&(bracket.expectingClose[bracket.expectingClose.length-1] != str[j])){
+       return false;
+     }
+  }
+  if (bracket.openCount == bracket.closeCount){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
